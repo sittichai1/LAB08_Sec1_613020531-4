68d8457ade847c4cce8de84ddc06107187321ca2
YuuklS <69790176+YuuklS@users.noreply.github.com>
2020-10-05 23:56:31 +0700

    ส่ง Lab 8ครับ

---
+++ app/src/main/java/com/example/myapplication/StudentAPI.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+
+import android.telecom.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface StudentAPI {
+    @GET("allstd")
+    fun retriveStudent(): retrofit2.Call<List<Student>>
+
+    @FormUrlEncoded
+    @POST("std")
+    fun insertStd(
+        @Field("std_id") std_id:String,
+        @Field("std_name") std_name:String,
+        @Field("std_age") std_age:Int
+    ):retrofit2.Call<Student>
+}
